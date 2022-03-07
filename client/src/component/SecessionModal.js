92b071a7ba132da2af67a46b0185f1a553849360
Park shinyeong <syngh503@gmail.com>
2022-03-08 02:07:07 +0900

    Hotfix: Fix secession modal

--- client/src/component/SecessionModal.js
+++ client/src/component/SecessionModal.js
@@ -1,12 +1,16 @@
-import React, { useState } from "react";
-import styled from "styled-components";
-import { useHistory } from "react-router";
-import { Link } from "react-router-dom";
-import github_icon from "../icon/github_icon.png";
-import { useSelector, useDispatch } from 'react-redux'; 
-import { setLogin, setLogout, setUpdateUserInfo} from "../redux/actions/actions";
-import warning from '../icon/warning.png'
-import axios from "axios";
+import React, { useState } from 'react';
+import styled from 'styled-components';
+import { useHistory } from 'react-router';
+import { Link } from 'react-router-dom';
+import github_icon from '../icon/github_icon.png';
+import { useSelector, useDispatch } from 'react-redux';
+import {
+  setLogin,
+  setLogout,
+  setUpdateUserInfo,
+} from '../redux/actions/actions';
+import warning from '../icon/warning.png';
+import axios from 'axios';
 axios.defaults.withCredentials = true;
 
 const Wrapper = styled.div`
@@ -50,11 +54,11 @@ const Wrapper = styled.div`
       margin: 30px auto;
       > .password {
       }
-      >span {
+      > span {
         display: inline-block;
         font-size: 14px;
         margin-top: 14px;
-        color:#a5a5a5;
+        color: #a5a5a5;
       }
       > .confirm_btn {
         width: 257px;
@@ -68,7 +72,7 @@ const Wrapper = styled.div`
         font-size: 15px;
         text-align: center;
         cursor: pointer;
-        border-radius:  10px;
+        border-radius: 10px;
         background-color: rgb(0, 0, 0, 0.1);
         color: rgb(0, 0, 0, 0.5);
         &:hover {
@@ -79,58 +83,57 @@ const Wrapper = styled.div`
       }
     }
   }
-  @media only screen and (max-width: 768px){
-      > .secession_modal {
-        width: 100%;
-        height: 100vh;
-        background-color: white;
-        position: relative;
-        top: 0;
+  @media only screen and (max-width: 768px) {
+    > .secession_modal {
+      width: 100%;
+      height: 100vh;
+      background-color: white;
+      position: relative;
+      top: 0;
+      box-sizing: border-box;
+      margin: 0 auto;
+      border-radius: 0px;
+      /* background: #fff; */
+      border: 1px solid #8b8585;
+      > img.icon {
+        padding: 80px 0 0 25px;
+      }
+      .title {
+      }
+      > .secession_box {
         box-sizing: border-box;
-        margin: 0 auto;
-        border-radius: 0px;
-        /* background: #fff; */
-        border: 1px solid #8b8585;
-        > img.icon {
-          padding: 80px 0 0 25px;
+        margin: 30px auto 0 auto;
+        > .password {
         }
-        .title {
+        > span {
+          display: block;
+          font-size: 14px;
+          margin-top: 14px;
+          color: #a5a5a5;
         }
-        > .secession_box {
-          box-sizing: border-box;
-          margin: 30px auto 0 auto;
-          > .password {
-          }
-          >span {
-            display: block;
-            font-size: 14px;
-            margin-top: 14px;
-            color:#a5a5a5;
-          }
       }
     }
   }
 `;
 
-
 const SecessionModal = ({ setIsSecessionModalOn }) => {
   const history = useHistory();
-  let setLoginState = useSelector((state) => state.setLoginReducer); 
-  let setUserInfo = useSelector((state) => state.setUserInfoReducer); 
+  let setLoginState = useSelector((state) => state.setLoginReducer);
+  let setUserInfo = useSelector((state) => state.setUserInfoReducer);
   let dispatch = useDispatch();
 
   // useState로 Input 값 받기
   let [inputPassword, setInputPassword] = useState('');
-  
+
   //Input 값 받는 함수
   const handleChangePassword = (e) => {
     setInputPassword(e.target.value);
-	};
+  };
 
-  //탈퇴하기 버튼 클리 시 진행되는 함수 
+  //탈퇴하기 버튼 클리 시 진행되는 함수
   const handleClickSecession = (e) => {
     // axios : 비밀번호 확인 후 탈퇴 진행
-    if(inputPassword !== '') {
+    if (inputPassword !== '') {
       axios
         .post(
           `${process.env.REACT_APP_API_URL}/login`,
@@ -144,28 +147,25 @@ const SecessionModal = ({ setIsSecessionModalOn }) => {
         )
         .then((data) => {
           axios
-            .delete(
-              `${process.env.REACT_APP_API_URL}/users/info`,
-              {
-                withCredentials: true,
-              }
-            )
-          .then((data) => {
-            alert('성공적으로 탈퇴되었습니다.')
-            dispatch(setLogout())
-            setIsSecessionModalOn(false);
-            history.push('/');
-          })
-          .catch((err) => {
-            alert('탈퇴되지 않았습니다.')
-            console.log(err);
-          });
+            .delete(`${process.env.REACT_APP_API_URL}/users/info`, {
+              withCredentials: true,
+            })
+            .then((data) => {
+              alert('성공적으로 탈퇴되었습니다.');
+              dispatch(setLogout());
+              setIsSecessionModalOn(false);
+              history.push('/');
+            })
+            .catch((err) => {
+              alert('탈퇴되지 않았습니다.');
+              console.log(err);
+            });
         })
         .catch((err) => {
           console.log(err);
         });
     }
-  }
+  };
 
   return (
     <Wrapper>
@@ -173,12 +173,12 @@ const SecessionModal = ({ setIsSecessionModalOn }) => {
         <div className="close" onClick={() => setIsSecessionModalOn(false)}>
           &times;
         </div>
-        <img className='icon' src={warning}/>
-        <div className='title'>회원 탈퇴</div>
+        <img className="icon" src={warning} />
+        <div className="title">회원 탈퇴</div>
         <div className="secession_box">
           <input
             className="password input_css"
-            // type="password"
+            type="password"
             placeholder="비밀번호를 입력해주세요."
             onChange={handleChangePassword}
           />
